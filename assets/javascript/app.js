d72203819cd04405560c53b90db7fad95c9ca878
kjige <chrisleongsc2@gmail.com>
2016-11-03 15:24:56 -0500

    reset and score

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,11 +7,15 @@ var ques = [
 
 	ques2 = {
 		'q': 'When?',
-		'a': ['Run', 'Walk', 'Bike', 'Dance']
+		'a': ['Run', 'Walk', 'Bike', 'Dance'],
+		'c': 1
 	}
 
 ]
 
+var quesNum = 0;
+var number = 6;
+var correct = ques[quesNum].c;
 startButton();
 
 // create start button
@@ -24,14 +28,32 @@ function startButton() {
 // when start button is clicked
 function clickStart() {
 	$('.start').click(function(){
-		$('.start').remove();
-		question();
+		$('.start').hide();
+		runTimer();
 	});
 }
 
-// load question
-var quesNum = 0;
+// set interval
+function runTimer() {
+	number = 6;
+    counter = setInterval(decrement, 1000);
+    $('.timer').html('Time remaining: ' + number);
+    question();
+}
+
+// decrease timer
+function decrement() {
+    number--;
+    $('.timer').html('Time remaining: ' + number);
+    if (number === 0) {
+        stop();
+        outOfTime();
+    }
+}
+
+// load question and answer choices
 function question() {
+	$('.main').empty();
 	var qu = $('<h3>').html(ques[quesNum].q).attr('class', 'ques');
 	$('.main').append(qu);
 	for (var i = 0; i < 4; i++) {
@@ -41,24 +63,7 @@ function question() {
 		});
 		$('.main').append(op);
 	}
-	runTimer();
-	checkAns();
-}
-
-// set interval
-function runTimer(){
-    counter = setInterval(decrement, 1000);
-}
-
-// The decrement function.
-var number = 3;
-function decrement(){
-    number--;
-    $('.timer').html('Time remaining: ' + number);
-    if (number === 0){
-        stop();
-        // outOfTime();
-    }
+	playerGuess();
 }
 
 // The stop function
@@ -66,18 +71,69 @@ function stop(){
     clearInterval(counter);
 }
 
+// when timer runs out
 function outOfTime() {
 	$('.main').html('Out of time!');
 }
 
-var correct = ques[quesNum].c;
-function wrong() {
-	var e = $('<p>').html('The correct answer is ' + ques[quesNum].a[correct]);
-	$('.main').html('Nope!').append(e);
+// click event listener for player guess
+var ansData;
+function playerGuess() {
+	$(document).on('click', '.list', function() {
+		ansData = $(this).data('ans');
+		checkAns();
+	})	
 }
 
+// check if player guess is right or wrong
 function checkAns() {
-	$('.list').click(function(){
-		
-	})
+		if (ansData === correct) {
+			correctAns();
+			console.log('correct');
+		}
+		if (ansData !== correct) {
+			wrongAns();
+			console.log('wrong');
+		}
+}
+
+// if player guessed right
+function correctAns() {
+	var e = $('<p>').html('The correct answer is ' + ques[quesNum].a[correct]);
+	$('.main').html('Correct!').append(e);
+	quesNum++;
+	stop();
+	wait();
+}
+
+// if player guessed wrong
+function wrongAns() {
+	var e = $('<p>').html('The correct answer is ' + ques[quesNum].a[correct]);
+	$('.main').html('Nope!').append(e);
+	quesNum++;
+	stop();
+	wait();
+}
+
+// wait to display next question
+function wait() {
+	setTimeout(runTimer, 1000*2);
+	checkQues();
+}
+
+// check if all questions answered
+function checkReset() {
+	if (quesNum === 2) {
+		score();
+	}
+}
+
+// display score to player
+function score() {
+
+}
+
+// when game ends
+function endGame() {
+
 }
