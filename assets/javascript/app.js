7e4b20215a804793a6bb262fc55f67f2a30b4ac1
kjige <chrisleongsc2@gmail.com>
2016-11-02 20:13:06 -0500

    check answer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,8 @@
 var ques = [
 	ques1 = {	
 		'q': 'Where?',
-		'a': ['Chicago', 'San Francisco', 'Cleveland', 'Kansas City']
+		'a': ['Chicago', 'San Francisco', 'Cleveland', 'Kansas City'],
+		'c': 0
 	},
 
 	ques2 = {
@@ -11,25 +12,72 @@ var ques = [
 
 ]
 
+startButton();
+
 // create start button
-var but = $('<button>').attr('class', 'btn btn-danger start').html('Start');
-$('.main').append(but);
-
-$('.start').click(function(){
-	$('.start').remove();
-	question();
-});
-
-// load question 1
-function question() {
-	var qu = $('<h3>').html(ques[0].q).attr('class', 'ques');
-	$('.main').append(qu);
-	for (var i = 0; i < 4; i++) {
-		var op = $('<p>').html(ques[0].a[i]).attr('class', 'btn btn-primary list');
-		$('.main').append(op);
-		setInterval();
-	}
+function startButton() {
+	var but = $('<button>').attr('class', 'btn btn-danger start').html('Start');
+	$('.title').append(but);
+	clickStart();
 }
 
+// when start button is clicked
+function clickStart() {
+	$('.start').click(function(){
+		$('.start').remove();
+		question();
+	});
+}
 
+// load question
+var quesNum = 0;
+function question() {
+	var qu = $('<h3>').html(ques[quesNum].q).attr('class', 'ques');
+	$('.main').append(qu);
+	for (var i = 0; i < 4; i++) {
+		var op = $('<p>').html(ques[quesNum].a[i]).attr({
+			'class': 'btn btn-primary list',
+			'data-ans': i
+		});
+		$('.main').append(op);
+	}
+	runTimer();
+	checkAns();
+}
 
+// set interval
+function runTimer(){
+    counter = setInterval(decrement, 1000);
+}
+
+// The decrement function.
+var number = 3;
+function decrement(){
+    number--;
+    $('.timer').html('Time remaining: ' + number);
+    if (number === 0){
+        stop();
+        // outOfTime();
+    }
+}
+
+// The stop function
+function stop(){
+    clearInterval(counter);
+}
+
+function outOfTime() {
+	$('.main').html('Out of time!');
+}
+
+var correct = ques[quesNum].c;
+function wrong() {
+	var e = $('<p>').html('The correct answer is ' + ques[quesNum].a[correct]);
+	$('.main').html('Nope!').append(e);
+}
+
+function checkAns() {
+	$('.list').click(function(){
+		
+	})
+}
